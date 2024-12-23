56c08e621336f4b9705bd70eb00cfab1744c8c6b
Michael Fleming <mikahl171@gmail.com>
2024-12-23 16:23:06 -0600

    final push now that all tests are functional and recorded

--- dist/server.js
+++ dist/server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', routes); // Make sure "/api" is the prefix for all routes
+app.use('/api', routes);
 db.once('open', () => {
     app.listen(PORT, () => {
         console.log(`API server running on port ${PORT}`);
